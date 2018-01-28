c08ce8d42ac13ae683d261fec256072d900940bf
noney1412 <nonpanpila@gmail.com>
2018-01-28 21:42:20 +0700

    do exe

---
+++ exercise/ex1.py
@@ -0,0 +1,12 @@
+
+value=input("count ")
+
+option_list = ["vowel","alphabet","digit","lowercase","uppercase"]
+
+optl = []
+if value in option_list :
+    optl.append(value)
+    print(optl)
+
+# print(x,y)
+
